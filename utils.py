bbd4353570a3dd5feea225a13b98f36e72dc3763
IvanArsenev <arsenev.ivan00@bk.ru>
2025-05-21 23:13:36 +0700

    Fix pylint

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ def get_memory_usage():
     Get system memory usage information.
 
     Returns:
-        tuple: Used memory in GB, total memory in GB, usage percentage
+        tuple: Used memory in GB: float, total memory in GB: float, usage percentage: float
     """
     mem = psutil.virtual_memory()
     total_gb = mem.total / (1024 ** 3)
@@ -24,9 +24,9 @@ def get_processors_info():
 
     Returns:
         list: Processors info and temperatures in the following format:
-            [processor1_name, processor1_load, processor1_frequency, processor1_temp,
-             processor2_name, processor2_load, processor2_frequency, processor2_temp,
-             motherboard_temp]
+            [processor1_name: str, processor1_load: int, processor1_frequency: int, processor1_temp: float,
+             processor2_name: str, processor2_load: int, processor2_frequency: int, processor2_temp: float,
+             motherboard_temp: float]
     """
     wmi_connection = wmi.WMI(namespace="root\\OpenHardwareMonitor")
     processors = wmi.WMI().Win32_Processor()
@@ -43,9 +43,12 @@ def get_processors_info():
         elif sensor.Identifier.startswith('/lpc/nct6779d/temperature'):
             motherboard_temp.append(sensor.Value)
 
-    avg_cpu1_temp = round(sum(cpu_1_cores_temp) / len(cpu_1_cores_temp), 1) if cpu_1_cores_temp else 0
-    avg_cpu2_temp = round(sum(cpu_2_cores_temp) / len(cpu_2_cores_temp), 1) if cpu_2_cores_temp else 0
-    avg_motherboard_temp = round(sum(motherboard_temp) / len(motherboard_temp), 1) if motherboard_temp else 0
+    avg_cpu1_temp = (round(sum(cpu_1_cores_temp) / len(cpu_1_cores_temp), 1)
+                     if cpu_1_cores_temp else 0)
+    avg_cpu2_temp = (round(sum(cpu_2_cores_temp) / len(cpu_2_cores_temp), 1)
+                     if cpu_2_cores_temp else 0)
+    avg_motherboard_temp = (round(sum(motherboard_temp) / len(motherboard_temp), 1)
+                           if motherboard_temp else 0)
 
     return [
         processors[0].Name.strip(),
